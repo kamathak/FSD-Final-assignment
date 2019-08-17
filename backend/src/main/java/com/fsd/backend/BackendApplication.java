46147833873d4a4a8a4ecf7f0bdbcd9fd0ac90ac
kamathak <ashwin.mite.com>
2019-08-17 20:52:29 +0530

    code changes for backend Project-Manager application

---
+++ backend/src/main/java/com/fsd/backend/BackendApplication.java
@@ -0,0 +1,28 @@
+package com.fsd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Class BackendApplication.
+ */
+
+/**
+ * @author AK
+ *
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fsd/backend/BackendTest.java
@@ -0,0 +1,21 @@
+package com.fsd.backend;
+
+import org.junit.Test;
+
+/**
+ * The Class BackendTest.
+ *
+ * @author AK
+ */
+public class BackendTest {
+
+	/**
+	 * Main.
+	 */
+
+	@Test
+	public void main() {
+		BackendApplication.main(new String[] {});
+	}
+
+}
