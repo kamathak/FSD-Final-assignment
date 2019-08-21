4a525034c4b3c3ceebfef9adebd1532464ea4662
kamathak <ashwin.mite.com>
2019-08-22 00:13:01 +0530

    Added frontend code

---
+++ frontend/src/app/pipe/order.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'order'
+})
+export class OrderPipe implements PipeTransform {
+  
+  transform(items: any[], field: string): any[] {
+    if (!items) return [];
+	if(field != "taskCompleted")  items.sort((a, b) => a.project[field]> b.project[field] ? 1 : -1);
+    else items.sort((a, b) => a > b ? 1 : -1);
+    return items;
+  }
+
+}
